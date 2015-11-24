8384a18b8614d6f424cd930a5b875941705d7d17
ndew4739 <n.dewilde@me.com>
2015-11-24 20:54:24 +1100

    Create fetchlevel.php

---
+++ fetchlevel.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if(!$_SESSION['user_login_status']==1){
+header("Location: main_login.php");
+}
+$username = $_SESSION['username'];
+
+$db = new mysqli('localhost', 'root', 'duckvin', 'myDb');
+
+if($db->connect_errno > 0){
+  die('Unable to connect to database [' . $db->connect_error . ']');
+}
+
+$sql = "
+SELECT doublelev
+FROM MyStudents
+WHERE username = '$username'";
+if(!$result = $db->query($sql)){
+  die('There was an error running the query [' . $db->error . ']');
+}
+
+$level = $result->fetch_assoc();
+echo $level['doublelev'];
+
+mysqli_close($conn);
+?>
